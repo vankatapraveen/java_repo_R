ec7ef12ab44702b16cf44639d440cac9a4763f87
vankatapraveen <vankatapraveen@gmail.com>
2023-03-30 19:16:06 +0530

    Code comitted by :praveen

---
+++ Hello.java
@@ -0,0 +1,18 @@
+public class Hello
+{
+public static void main(String args[])
+{
+int num1=20;
+int num2=21;
+
+int sum=num1+num2;
+
+System.out.println("===The result should be sum of num1 and num2=====");
+
+System.out.println(sum);
+
+}
+}
+
+
+
